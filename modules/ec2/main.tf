5f7f7cdb9bc9704fc3c10d0ddefef4d5ed466cc0
Maryna Nazarenko <maryna_nazarenko1@epam.com>
2024-05-21 21:10:09 +0200

    trying to figure out the origin of docker-compose template that tf uses

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -10,6 +10,8 @@ locals {
             sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
             sudo chmod +x /usr/local/bin/docker-compose
             echo "export IMAGE_TAG=IMAGE_TAG" >> /etc/environment
+            echo "Docker-compose file path: ${path.module}/templates/docker-compose.yaml" >> /etc/environment
+
             EOF
 
 }
