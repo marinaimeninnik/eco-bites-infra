0b2b3b38bdcb2139946976bab2ac592db764f730
Maryna Nazarenko <maryna_nazarenko1@epam.com>
2024-05-23 19:31:48 +0200

    add step with uploading generated docker-compose

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -9,7 +9,6 @@ locals {
             sudo service docker restart
             sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
             sudo chmod +x /usr/local/bin/docker-compose
-            echo "export IMAGE_TAG=IMAGE_TAG" >> /etc/environment
 
             EOF
 
