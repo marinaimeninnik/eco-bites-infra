6ff6555036e398d4b6c4aef4ce9f32c11d832c71
Maryna Nazarenko <maryna_nazarenko1@epam.com>
2024-05-26 21:58:32 +0200

    s3 force destroy value changes to true

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -19,7 +19,7 @@ variable "bucket_prefix" {
 variable "force_destroy" {
   description = "(Optional, Default:false ) A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "object_lock_enabled" {
