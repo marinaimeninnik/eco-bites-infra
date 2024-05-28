bcda00c92d63fa86f652a2cd4300be720cc5ea31
Maryna Nazarenko <maryna_nazarenko1@epam.com>
2024-05-28 20:24:32 +0200

    aws cli installation added to user_data block

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -9,6 +9,7 @@ locals {
             sudo service docker restart
             sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
             sudo chmod +x /usr/local/bin/docker-compose
+            sudo apt-get -y install awscli
 
             EOF
 
