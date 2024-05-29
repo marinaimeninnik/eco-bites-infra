9f4a18d71bb63bf327435e896bc8db8f03505449
Maryna Nazarenko <maryna_nazarenko1@epam.com>
2024-05-29 16:36:38 +0200

    user data added to match needs

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -17,7 +17,8 @@ locals {
   user_data = <<-EOF
             #!/bin/bash
             sudo yum -y update
-            sudo amazon-linux-extras install docker
+            sudo yum install -y docker
+            sudo yum install -y libxcrypt-compat   #library for docker-compose execution
             sudo service docker start
             sudo usermod -a -G docker ec2-user
             sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
